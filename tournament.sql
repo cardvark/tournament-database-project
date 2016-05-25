f00c20727dbcaba4fd5281e5ad943d264b2131f3
James Wang <jameswang@Huis-MacBook-Air-3.local>
2016-05-24 17:02:17 -0700

    Initialized DB, first pass creating tables.  Players, Matches, Results

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,32 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- create database tournament;
 
+create table players (
+    p_id serial primary key,
+    name text,
+    score integer default 0
+);
+
+create table matches (
+    m_id serial primary key,
+    round integer,
+    player_1 integer references players (p_id),
+    player_2 integer references players (p_id)
+);
+
+create table results (
+    m_id integer references matches,
+    p_id integer references players,
+    result text
+);
+
+/*
+
+tables:
+- players: id (PK), name, score.  what else?
+- matches: match ID (PK), round, p id1 (FK), p id2 (FK)
+- results: match ID (FK),  p id (FK), result (win, lose, draw)
+
+*/
